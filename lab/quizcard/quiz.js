294762f51c1f10d92d53e4f3b63e3fbca7db4844
Todd <toddpastell@gmail.com>
2014-08-17 22:53:59 -0700

    quizcards

--- lab/quizcard/quiz.js
+++ lab/quizcard/quiz.js
@@ -1,12 +1,18 @@
 function Quiz(file) {
+	var quiz = this;
 	this.questionElement = document.getElementById('question');
 	this.answersElement = document.getElementById('answers');
+	this.questions = [];
+	
+	this.nextQuestion = function() {
+		console.log( this.questions );
+	};
 	
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
-		console.log(xmlhttp);
-		if(xmlhttp.readyState == 4 && xmlhttp.status==200) {
-			console.log(JSON.parse(xmlhttp.response));
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			quiz.questions = JSON.parse(xmlhttp.response);
+			quiz.nextQuestion();
 		}
 	}
 	xmlhttp.open('GET', file, true);
