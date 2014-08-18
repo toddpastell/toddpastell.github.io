fc01698be49aae930a9e42e9889e846a06b0a5c4
Todd <toddpastell@gmail.com>
2014-08-17 23:12:12 -0700

    quizcards

--- lab/quizcard/quiz.js
+++ lab/quizcard/quiz.js
@@ -3,9 +3,27 @@ function Quiz(file) {
 	this.questionElement = document.getElementById('question');
 	this.answersElement = document.getElementById('answers');
 	this.questions = [];
+	this.correctAnswer;
 	
 	this.nextQuestion = function() {
-		console.log( this.questions );
+		while(this.answersElement.hasChildNodes()) {
+			this.answersElement.removeChild(this.answersElement.lastChild);
+		}
+		var randomCorrectIndex = Math.floor(Math.random() * 4);
+		for(var i = 0; i < 4; i++) {
+			var randomIndex = Math.floor(Math.random() * this.questions.length);
+			var question = this.questions[randomIndex];
+			if(i == randomCorrectIndex) {
+				this.correctAnswer = question.b;
+				this.questionElement.innerHTML = question.a;
+			}
+			var answer = new Answer(this, question.b);
+			this.answersElement.appendChild(answer.element);
+		}
+	};
+	
+	this.checkAnswer(answer) {
+		alert(answer);
 	};
 	
 	var xmlhttp = new XMLHttpRequest();
@@ -17,4 +35,15 @@ function Quiz(file) {
 	}
 	xmlhttp.open('GET', file, true);
 	xmlhttp.send();
+}
+
+function Answer(quiz, answer) {
+	this.element = document.createElement('a');
+	this.element.setAttribute('class', 'btn');
+	this.element.setAttribute('href', '#' + nextPage);
+	this.element.innerHTML = text;
+	this.element.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		quiz.checkAnswer(answer);
+	});
 }
