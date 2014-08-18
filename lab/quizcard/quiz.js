1f0c7898bd82a059900e0131b7ce1d2ab0fad565
Todd <toddpastell@gmail.com>
2014-08-17 22:48:18 -0700

    quizcards

--- lab/quizcard/quiz.js
+++ lab/quizcard/quiz.js
@@ -5,6 +5,9 @@ function Quiz(file) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		console.log(xmlhttp);
+		if(xmlhttp.readyState == 4 && xmlhttp.status==200) {
+			console.log(JSON.parse(xmlhttp.response));
+		}
 	}
 	xmlhttp.open('GET', file, true);
 	xmlhttp.send();
